1f9860ea79da9d5a5c1e01a832db3da2972d95ef
rrcorreia <rrcorreia.co@gmail.com>
2022-12-05 10:50:57 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+
+const movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
+function App() {
+  return (
+    <div className="App">
+      movies
+    </div>
+  );
+}
+
+export default App;
